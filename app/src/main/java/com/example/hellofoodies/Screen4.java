ae3c4b374f45f9ee1b0db1409b862d42c5e252d4
Padmapriya-09 <priyav4599@gmail.com>
2020-07-09 16:10:28 +0530

    Initial Commit

--- app/src/main/java/com/example/hellofoodies/Screen4.java
+++ app/src/main/java/com/example/hellofoodies/Screen4.java
@@ -2,10 +2,14 @@ package com.example.hellofoodies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -13,6 +17,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,42 +28,31 @@ public class Screen4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen4);
 
-        /*Button proceed_button = (Button) findViewById(R.id.button1);
-        proceed_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                URL url = null;
-                try {
-                    url = new URL(" https://mekvahan.com/api/android_intern_task");
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-                HttpURLConnection con = null;
-                try {
-                    con = (HttpURLConnection) url.openConnection();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    con.setRequestMethod("GET");
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                }
+        final EditText m_number = (EditText) findViewById(R.id.ed2);
 
-                Map<String, String> parameters = new HashMap<>();
-                parameters.put("param1", "val");
+        final EditText password = (EditText) findViewById(R.id.password);
 
-                con.setDoOutput(true);
-                DataOutputStream out = null;
-                try {
-                    out = new DataOutputStream(con.getOutputStream());
-                } catch (IOException e) {
-                    e.printStackTrace();
+        Button proceed_button = (Button) findViewById(R.id.proceed_button);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            proceed_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String mobile_num = m_number.getText().toString();
+                    String pass = password.getText().toString();
+
+                    String number="9999999999";
+                    String passcode="0123456789";
+
+                    if(mobile_num.equals(number) && pass.equals(passcode))
+                    {
+                        Toast.makeText(getApplicationContext(), R.string.booking_successful, Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), R.string.incorrect_password, Toast.LENGTH_LONG).show();
+                    }
                 }
-                out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
-                out.flush();
-                out.close();
-            }
-        });*/
+            });
+        }
     }
 }
